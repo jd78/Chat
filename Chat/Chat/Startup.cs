6775fcb013f652d16e5eb1fa723eb6c2b69adc6a
jd78 <gennaro.delsorbo@gmail.com>
2014-01-21 19:35:38 +0000

    Files added

---
+++ Chat/Chat/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Chat.Startup))]
+namespace Chat
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
